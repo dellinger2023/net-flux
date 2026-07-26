470967df36db560de7e7e08b44738c7b31070b01
zhangxinyu <zhangxinyu@agora.io>
2026-07-26 11:11:55 +0800

    add cache facility

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -12,3 +12,13 @@ import "time"
 func NowString() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
+
+/**
+* get the current date string
+* @return: the current date string
+* @example
+* - NowDateString() -> "2026-01-06"
+ */
+func NowDateString() string {
+	return time.Now().Format("2006-01-02")
+}
